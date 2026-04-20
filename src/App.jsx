81c3c036dafe960902cd3f1aadc2e2021a1f8bb6
CJ Tee <90126284+CeeJank@users.noreply.github.com>
2026-04-20 17:08:48 +0800

    cleared out original

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Header from '@/components/common/Header';
+import Landing from '@/pages/landing/Landing';
+import Login from '@/pages/landing/Login';
+import Signup from '@/pages/landing/Signup';
 
 
 function App() {
-  
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+
+    </BrowserRouter>
+    </>
+
+
+
+  );
   
 }
 
-export default App
+export default App;
