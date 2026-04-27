bacfb0cca20dad8770f9ca6a102baefd3a8078f2
CJ <90126284+CeeJank@users.noreply.github.com>
2026-04-27 09:42:29 +0800

    fix password input

--- src/pages/landing/Login.jsx
+++ src/pages/landing/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
         <>
         <h2>Login Page</h2>
         <Textinput name="username" label="name" placeholder="John Smith" minLength="10" required></Textinput>
-        <Textinput name="password" placeholder="12345678" minLength="8"></Textinput>
+        <Textinput name="password" labe="password" placeholder="12345678" minLength="8" required></Textinput>
         <p>Remember my login<input type="checkbox" name="checkbox"></input></p> 
         <Button className="primary">Login</Button>
 
