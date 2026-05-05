331e99d6fc3b3e72d99502cd944ac6572a2f2785
CJ <90126284+CeeJank@users.noreply.github.com>
2026-05-05 23:32:19 +0800

    added list of other signup methods

---
+++ src/components/OtherLoginSignup.jsx
@@ -0,0 +1,22 @@
+import {Link} from 'react-router';
+import {Button} from '@/components/common/Buttons';
+import UnorderedList from '@/components/common/UnorderedList';
+
+export default function OtherLoginSignup () {
+    const launchWindow = () => {
+        const url = '';
+        const type = "popupwindow";
+        const size = "width=500,height=400";
+        window.open(url, type, size);
+    }
+
+    return(
+        <div>
+            <ul style={{listStyleType: 'none'}}>
+                <li><Button onClick={launchWindow}>Github</Button></li>
+                <li><Button onClick={launchWindow}>Google</Button></li>
+                <li><Button onClick={launchWindow}>Apple</Button></li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/common/UnorderedList.jsx
@@ -0,0 +1,7 @@
+export default function UnorderedList({style, children}) {
+    return (
+            <ul style={style}>
+                {children}
+            </ul>
+    )
+}
