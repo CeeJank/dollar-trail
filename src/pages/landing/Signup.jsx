798139b4d2e9c0ffe04b18144a0f60d7d8f6a513
CJ <90126284+CeeJank@users.noreply.github.com>
2026-05-05 23:33:04 +0800

    set up sign up page structure

--- src/pages/landing/Signup.jsx
+++ src/pages/landing/Signup.jsx
@@ -1,16 +1,52 @@
-import {Textinput} from '@/components/common/Textinput';
+import Textinput from '@/components/common/Textinput';
+import {Button} from '@/components/common/Buttons';
+import OtherLoginSignup from '@/components/OtherLoginSignup';
+import {useState} from  'react';
 
 
 export default function Signup() {
+    // states to set errors to input fields
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState({});
 
+    // checks for username length
     const validateSignUp = () => {
+        const errors = {};
+        if (username.length == 0) errors.username = "Username cannot be empty!!"
+        if (password.length == 0) errors.password = "password cannot be empty!!"
+        return errors;
+    }
 
+    // validate on type for username and password, giving user feedback along if input is too short etc
+    const handleUsernameChange = (e) => {
+        const username = e.target.value;
+        setUsername(username); // displays the input typed
+        if (username.length < 8) {
+            setError(prev => ({...prev, username: 'Username Too short'}));
+        } else {
+            setError(prev => ({...prev, username: ''}));
+        }
+    }
+
+    // same as above but password
+    const handlePasswordChange = (e) => {
+        const password = e.target.value;
+        setPassword(password); 
+        if (password.length < 8) {
+            setError(prev => ({...prev, password: 'password too short!'}))
+        } else {
+            setError(prev => ({...prev, password: ''}))
+        }
     }
 
     const handleSubmit = () => {
+        const validation = validateSignUp();
 
+        if (Object.keys(validation).length > 0) {
+                setError(validation);
+                return;
+        }
     }
 
     return (
@@ -22,8 +58,8 @@ export default function Signup() {
                 label="name" 
                 placeholder="John Smith" 
                 required 
-                onChange={(e) => setUsername(e.target.value)} 
-                error={errors.username}
+                onChange={handleUsernameChange} 
+                error={error.username}
             ></Textinput>
 
             <Textinput 
@@ -32,14 +68,14 @@ export default function Signup() {
                 label="password" 
                 placeholder="12345678" 
                 required 
-                onChange={(e) => setPassword(e.target.value)} 
-                error={errors.password}
+                onChange={handlePasswordChange} 
+                error={error.password}
             ></Textinput>
 
-            <Button onClick = {handleSubmit}>Sign In</Button>
+            <Button className="primary" onClick = {handleSubmit}>Sign In</Button>
 
             <p>Other methods:</p>
-
+            <OtherLoginSignup />
         </>
     );
 }
