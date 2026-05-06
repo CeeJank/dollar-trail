873d2ad6f2177ead1e4bb9ff857473e1f7b1e2a6
CJ Tee <90126284+CeeJank@users.noreply.github.com>
2026-05-07 00:20:20 +0800

    add 1 line

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/common/Buttons';
 import Footer from '@/components/common/Footer';
+import Navbar from '@/layouts/Navbar';
 
 export default function Landing() {
     return (
         <div>
+            <Navbar />
             <h1>Welcome to Dollar Trail!</h1>
+            <p>Your best companion to tracking expenses on the go!</p>
             <Button className="primary" to="/login">Login</Button>
             <Button className="secondary" to="/signup">Sign Up</Button>
 
