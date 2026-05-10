9016fcdc85823b9f2e927f44052f76f7a2b33370
CJ Tee <90126284+CeeJank@users.noreply.github.com>
2026-05-11 00:17:28 +0800

    centering footer at bottom

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ import UnorderedList from "@/components/common/UnorderedList";
 
 export default function Footer() {
     return(
-        <footer className="footer-wrapper" position="fixed">
+        <footer className="flex fixed bottom-0 justify-center mx-auto w-full">
             <div className="brand-side">
                 <img src="src/assets/logo.jpg" alt="dollar-trail logo" height="200" width="200"></img>
                 <h3>Dollar-trail</h3>
